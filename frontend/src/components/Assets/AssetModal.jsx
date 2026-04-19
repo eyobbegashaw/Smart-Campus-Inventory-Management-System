df7faa3bfaaf351e9fad1eb19e638a14fac3687e
Eyob Begashaw Ayele <eyobbegashaw075@gmail.com>
2026-04-20 06:30:33 +1200

    Update AssetModal.jsx

--- frontend/src/components/Assets/AssetModal.jsx
+++ frontend/src/components/Assets/AssetModal.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { FiX, FiUpload, FiDollarSign, FiTag, FiMapPin, FiCalendar, FiHash } from 'react-icons/fi';
