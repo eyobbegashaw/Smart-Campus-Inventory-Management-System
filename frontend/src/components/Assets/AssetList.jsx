e861fb0850191e762cd7db50564b9662661a7aa2
Eyob Begashaw Ayele <eyobbegashaw075@gmail.com>
2026-04-24 20:14:25 +0300

    Update AssetList.jsx

--- frontend/src/components/Assets/AssetList.jsx
+++ frontend/src/components/Assets/AssetList.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useMemo } from 'react';
 import { useTable, usePagination, useSortBy, useFilters } from 'react-table';
 import { FiSearch, FiFilter, FiDownload, FiCamera, FiEye } from 'react-icons/fi';
