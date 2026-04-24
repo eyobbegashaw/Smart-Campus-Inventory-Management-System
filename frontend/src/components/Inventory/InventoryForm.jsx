f7562f21b8b6cb4ea263293beae255a7ade288ba
Eyob Begashaw Ayele <eyobbegashaw075@gmail.com>
2026-04-24 20:18:29 +0300

    Update InventoryForm.jsx

--- frontend/src/components/Inventory/InventoryForm.jsx
+++ frontend/src/components/Inventory/InventoryForm.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { FiX, FiTag, FiMapPin, FiDollarSign, FiPackage, FiAlertCircle } from 'react-icons/fi';
