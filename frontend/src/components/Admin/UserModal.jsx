05c6887e5c9824cf323a319cafc265796b21d058
Eyob Begashaw Ayele <eyobbegashaw075@gmail.com>
2026-04-30 09:17:00 -1200

    Update UserModal.jsx

--- frontend/src/components/Admin/UserModal.jsx
+++ frontend/src/components/Admin/UserModal.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { FiX, FiMail, FiLock, FiUser, FiPhone, FiBriefcase } from 'react-icons/fi';
