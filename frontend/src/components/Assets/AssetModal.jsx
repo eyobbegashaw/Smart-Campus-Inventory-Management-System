4ce3ad41b7f1292ae0a51bb8c78f20e1d0f48af7
Eyob Begashaw Ayele <eyobbegashaw075@gmail.com>
2026-05-15 19:44:04 +0300

    Remove empty lines at the beginning of AssetModal.jsx

--- frontend/src/components/Assets/AssetModal.jsx
+++ frontend/src/components/Assets/AssetModal.jsx
@@ -1,5 +1,3 @@
-
-
 import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { FiX, FiUpload, FiDollarSign, FiTag, FiMapPin, FiCalendar, FiHash } from 'react-icons/fi';
